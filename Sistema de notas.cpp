ca0845e24926c0c07832d1203798ce9ad72af9a3
rcedenod <134077200+rcedenod@users.noreply.github.com>
2023-07-15 11:48:22 -0400

    feat:Sistema de notas:laboratorio
    Añadida la opcion para sacar promedio de notas por cada materia.

--- Sistema de notas.cpp
+++ Sistema de notas.cpp
@@ -517,6 +517,71 @@ void anadir_estudiante()
     }
 }
 
+void promedio_por_materia()
+{
+    ifstream infile;
+    infile.open("Datos_Estudiantes.csv");
+    if (!infile)
+    {
+        cout << "Error al abrir el infile" << endl;
+    }
+    else
+    {
+        string linea, nombre, apellido, email, genero, matematicas, sociales, biologia, fisica, educ_fisica, artes;
+        float suma_mate = 0, suma_sociales = 0, suma_biologia = 0, suma_fisica = 0, suma_ef = 0, suma_artes = 0, numestudiantes = 0;
+        float promedio_mate = 0, promedio_sociales = 0, promedio_biologia = 0, promedio_fisica = 0, promedio_ef = 0, promedio_artes = 0;
+        int nota = 0;
+
+        getline(infile, linea);
+
+        while (getline(infile, linea))
+        {   
+            stringstream token(linea);
+            getline(token, nombre, ';');
+            getline(token, apellido, ';');
+            getline(token, email, ';');
+            getline(token, genero, ';');
+            getline(token, matematicas, ';');
+            getline(token, sociales, ';');
+            getline(token, biologia, ';');
+            getline(token, fisica, ';');
+            getline(token, educ_fisica, ';');
+            getline(token, artes, '\n');
+
+            numestudiantes++;
+            nota = stoi(matematicas);
+            suma_mate += nota;
+            nota = stoi(sociales);
+            suma_sociales += nota;
+            nota = stoi(biologia);
+            suma_biologia += nota;
+            nota = stoi(fisica);
+            suma_fisica += nota;
+            nota = stoi(educ_fisica);
+            suma_ef += nota;
+            nota = stoi(artes);
+            suma_artes += nota;
+
+        }
+
+        promedio_mate = suma_mate / numestudiantes;
+        promedio_sociales = suma_sociales / numestudiantes;
+        promedio_biologia = suma_biologia / numestudiantes;
+        promedio_fisica = suma_fisica / numestudiantes;
+        promedio_ef = suma_ef / numestudiantes;
+        promedio_artes = suma_artes / numestudiantes;
+
+        cout << endl << "-- Promedios de cada materia --" << endl
+             << "Matematicas :" << promedio_mate << endl
+             << "Sociales : " << promedio_sociales << endl
+             << "Biologia: " << promedio_biologia << endl
+             << "Fisica: " << promedio_fisica << endl
+             << "Educacion fisica: " << promedio_ef << endl
+             << "Artes: " << promedio_artes << endl; 
+    }
+    infile.close();
+}
+
 int main()
 {
 
@@ -583,7 +648,7 @@ int main()
             break;
 
         case 5:
-            
+            promedio_por_materia();
             break;
             
         case 6:
