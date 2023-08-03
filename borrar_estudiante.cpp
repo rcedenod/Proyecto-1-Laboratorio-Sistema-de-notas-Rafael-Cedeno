22843487827fb8d31c434e56a37a4f859da25b35
rcedenod <134077200+rcedenod@users.noreply.github.com>
2023-08-03 17:09:10 -0400

    (bugfix)
    El separador estaba puesto con ';' como en el archivo anterior y lo cambie a ','

---
+++ borrar_estudiante.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+void borrar_estudiante() {
+    ifstream infile("datos_estudiantes(2).csv");
+    ofstream outfile("datos_estudiantes(2)_auxiliar.csv");
+    if (!infile || !outfile) {
+        cout << "Error al abrir el archivo" << endl;
+    } else {
+        string linea, id, nombre, apellido, genero, email, seccion, matematicas, sociales, biologia, educ_fisica, artes;
+        bool existe = false;
+
+        cout << "Ingrese el ID del estudiante que desea borrar: ";
+        int id_buscar;
+        cin >> id_buscar;
+
+        getline(infile, linea);
+        outfile << linea << endl;
+
+        while (getline(infile, linea)) {
+            stringstream token(linea);
+            getline(token, id, ',');
+
+            if (id.empty()) {
+                break;
+            }
+
+            getline(token, nombre, ',');
+            getline(token, apellido, ',');
+            getline(token, genero, ',');
+            getline(token, email, ',');
+            getline(token, seccion, ',');
+            getline(token, matematicas, ',');
+            getline(token, sociales, ',');
+            getline(token, biologia, ',');
+            getline(token, educ_fisica, ',');
+            getline(token, artes, '\n');
+
+            if (!(stoi(id) == id_buscar)) {
+                outfile << id << "," << nombre << "," << apellido << "," << genero << "," << email << "," << seccion << "," << matematicas
+                         << "," << sociales << "," << biologia << "," << educ_fisica << "," << artes << endl;
+            }else{
+                 cout << "Se ha borrado el estudiante del sistema" << endl;
+                 existe = true;
+            }   
+        }
+
+        if (!existe) {
+            cout << "El estudiante con ID: " << id_buscar << " no se encuentra en el sistema" << endl;
+        } 
+
+        infile.close();
+        outfile.close();
+        }
+        remove("datos_estudiantes(2).csv");
+        rename("datos_estudiantes(2)_auxiliar.csv", "datos_estudiantes(2).csv");
+    }
+
